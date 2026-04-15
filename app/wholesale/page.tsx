170f33db7a53aefafffe178c0d15632cfb15e4b7
LEE MI YONG <jmjmanse@naver.com>
2026-04-16 06:21:25 +0900

    rename temp

--- app/wholesale/page.tsx
+++ app/wholesale/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import WholesalePageClient from "./WholesalePageClient";
+import WholesalePageClient from "./temp";
 
 export default function WholesalePage() {
   return (
